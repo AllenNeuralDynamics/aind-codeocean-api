01416d930c711a2be899414298a2cebf66472c92
aricAI <115503636+aricAI@users.noreply.github.com>
2023-01-19 12:32:35 -0800

    test: attempt to fix test

--- tests/test_codeocean_requests.py
+++ tests/test_codeocean_requests.py
@@ -651,32 +651,32 @@ class TestCodeOceanDataAssetRequests(unittest.TestCase):
         self, mock_api_post: unittest.mock.MagicMock
     ) -> None:
         """Tests the response of updating permissions"""
-        input_json_data = {}
-        capsule_id = "648473aa-791e-4372-bd25-205cc587ec56"
-        permissions = {
-            "users": [
-                {"id": "user1", "role": "admin"},
-                {"id": "user2", "role": "viewer"}
-            ],
-            "groups": [
-                {"id": "group3", "role": "admin"},
-                {"id": "group4", "role": "viewer"}
-            ]
-        }
 
         def map_to_success_message(input_json: dict) -> dict:
             """Map to a success message"""
             success_message = {}
             return success_message
+        
+        permissions = {
+            "users": [
+                {"id": "user2", "role": "viewer"}
+            ],
+            "groups": [
+                {"id": "group4", "role": "viewer"}
+            ]
+        }
+        example_capsule_id = "648473aa-791e-4372-bd25-205cc587ec56"
+        input_json_data = {"capsule_id": example_capsule_id, "permissions": []}
 
-        expected_status_code = 200
+        mocked_success_post = self.mock_success_response(
+            map_to_success_message, req_type="post"
+        )
+        mock_api_post.return_value = mocked_success_post(json=input_json_data)
 
-        mock_response = self.mock_success_response(map_to_success_message, req_type="post")
-
-        mock_api_post.return_value = mock_response(json=input_json_data)
-
-        response = self.co_client.update_permissions(capsule_id, permissions)
-        self.assertEqual(response.status_code, expected_status_code)
+        response = self.co_client.update_permissions(
+            capsule_id=example_capsule_id, permissions=permissions
+        )
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
