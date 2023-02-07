13efc151a9d549c3a544b475c4da07b35476f5e6
aricAI <115503636+aricAI@users.noreply.github.com>
2023-02-07 11:50:29 -0800

    test: work on tests, add dependency

--- tests/test_credentials_in_aws.py
+++ tests/test_credentials_in_aws.py
@@ -18,26 +18,21 @@ FAKE_PATH_USER_INPUT = TEST_DIR / "resources"
 class TestCredentialsInAWS(unittest.TestCase):
     """Tests credentials in AWS methods."""
 
+    def test__get_secret():
+        """Tests _get_secret method."""
+        pass
 
-    def test_create_credentials_from_aws(self):
+    @patch("json.dump")
+    @patch("builtins.open", new_callable=unittest.mock.mock_open())
+    def test_create_credentials_from_aws(self, m, m_json):
         """Tests that credentials are loaded from AWS."""
+        domain = "https://acmecorp.codeocean.com"
+        token = "fake_token"
         
         credentials_in_aws.create_credentials_from_aws_secrets_manager(
             secret_name="test_secret",
             file_location="mock_file.json"
         )
-
-        co_creds = CodeOceanCredentials()
-        self.assertEqual({"token": "a_fake_token"}, co_creds.credentials)
-
-    @patch("json.dump")
-    @patch("builtins.open", new_callable=unittest.mock.mock_open())
-    def test_create_credentials(self, m, m_json):
-        """Tests create_credentials method."""
-        domain = "https://acmecorp.codeocean.com"
-        token = "fake_token"
-
-        create
 
         m.assert_called_once_with("mock_file.json", "w+")
         m_json.assert_called_with(
@@ -45,4 +40,5 @@ class TestCredentialsInAWS(unittest.TestCase):
             m.return_value.__enter__.return_value,
             indent=4,
         )
-
+        co_creds = CodeOceanCredentials()
+        self.assertEqual({"token": "a_fake_token"}, co_creds.credentials)
