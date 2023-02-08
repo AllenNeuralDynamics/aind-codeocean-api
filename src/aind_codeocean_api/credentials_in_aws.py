f215a3cd08c9de81fe8f18ae78d300c16aa3052d
aricAI <115503636+aricAI@users.noreply.github.com>
2023-02-07 17:22:03 -0800

    test: working on tests

--- src/aind_codeocean_api/credentials_in_aws.py
+++ src/aind_codeocean_api/credentials_in_aws.py
@@ -5,27 +5,63 @@ import os
 from pathlib import Path
 
 import boto3
-import botocore.session 
 from botocore.exceptions import ClientError
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 
-import aind_codeocean_api.credentials as credentials
+import aind_codeocean_api.credentials as co_credentials
+
+AWS_ROLE_ARN = "arn:aws:iam::467914378000:role/codeocean-secret-retriever"
 
 
-def _get_secret(secret_name: str) -> str:
-    """Get secret from AWS Secrets Manager."""
-    # session = boto3.session.Session()
-    # client = session.client(
-    #     service_name="secretsmanager",
-    #     region_name=os.environ.get("AWS_REGION", "us-west-2"),
-    # )
-
-    client = botocore.session.get_session().create_client('secretsmanager')
+def _get_secrets_cache_client(credentials: dict = None):
+    """
+    Get a boto3 client for AWS Secrets Manager,
+    using SecretCache to improve performance.
+    """
+    if credentials is None:
+        credentials = _get_credentials()
+    session = boto3.session.Session(
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken'],
+    )
+    client = session.client(
+        service_name="secretsmanager",
+        region_name=os.environ.get("AWS_REGION", "us-west-2"),
+    )
     cache_config = SecretCacheConfig()
-    cache = SecretCache( config = cache_config, client = client)
+    cache = SecretCache(
+        config = cache_config,
+        client = client,
+    )
+    return cache
+
+def _get_credentials(aws_role_arn: str = AWS_ROLE_ARN):
+    """
+    Get credentials for the given AWS role.
+    The environment must be authorized to AWS.
+    """
+    credentials = None
+    try:
+        assumed_role_object = boto3.client("sts").assume_role(
+            RoleArn=aws_role_arn,
+            RoleSessionName="AssumeCodeoceanRoleSession",
+        )
+    except ClientError as e:
+        print("Error assuming role:", e)
+        raise
+    else:
+        credentials=assumed_role_object['Credentials']
+        return credentials
+
+def _get_secret(secret_name: str, credentials: dict = None) -> str:
+    """Get secret from AWS Secrets Manager."""
+    secret = None
+    cache = _get_secrets_cache_client(credentials)
 
     try:
-        get_secret_string_response = cache.get_secret_string(
+        print("Retrieving secret '" + secret_name + "' from AWS Secrets Manager")
+        secret = cache.get_secret_string(
             secret_id=secret_name
         )
     except ClientError as e:
@@ -39,35 +75,28 @@ def _get_secret(secret_name: str) -> str:
             print("The request was invalid due to:", e)
         elif e.response["Error"]["Code"] == "ResourceNotFoundException":
             print("Secret named '" + secret_name + "' not found")
-        # raise e
     else:
         print("Secret '" + secret_name + "' found")
-        print("Response: " + get_secret_string_response)
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if "SecretString" in get_secret_string_response:
-            secret = get_secret_string_response["SecretString"]
-        else:
-            secret = base64.b64decode(get_secret_string_response["SecretBinary"])
+        print("Secret value: " + secret)
     
     return secret
 
 
 def create_credentials_from_aws_secrets_manager(
-    secret_name: str,
+    secret_name: str = "codeocean-service-account",
     file_location: str = None,
 ) -> None:
     """Create CodeOceanCredentials from AWS Secrets Manager."""
     if file_location is None:
-        file_location = str(credentials.DEFAULT_HOME_PATH)
+        file_location = str(co_credentials.DEFAULT_HOME_PATH)
     else:
-        file_location = os.path.join(file_location, credentials.CREDENTIALS_FILENAME)
+        file_location = os.path.join(file_location, co_credentials.CREDENTIALS_FILENAME)
 
     Path(file_location).parent.mkdir(exist_ok=True, parents=True)
 
     secret = _get_secret(secret_name)
     secret_dict = json.loads(secret)
-    credentials.CodeOceanCredentials.create_credentials(
+    co_credentials.CodeOceanCredentials.create_credentials(
         secret_dict["api_domain"],
         secret_dict["access_token"],
         file_location,
--- tests/test_credentials_in_aws.py
+++ tests/test_credentials_in_aws.py
@@ -1,26 +1,55 @@
 """Test credentials in AWS"""
 import os
 import sys
+import json
 import unittest
 from pathlib import Path
 from unittest import mock
 from unittest.mock import patch
 
+from aws_secretsmanager_caching import SecretCache
+import boto3
+import botocore
+from moto import mock_secretsmanager, mock_sts
+
 from aind_codeocean_api.credentials import CodeOceanCredentials
 from aind_codeocean_api import credentials_in_aws
-
 
 TEST_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 FAKE_CREDENTIALS_PATH = TEST_DIR / "resources" / "fake_credentials.json"
 FAKE_PATH_USER_INPUT = TEST_DIR / "resources"
+AWS_ROLE_ARN = "arn:aws:iam::467914378000:role/codeocean-secret-retriever"
+orig = botocore.client.BaseClient._make_api_call
 
+
+class MockSecretCache:
+    """Mock of the boto3 secrets cache client."""
+    def get_secret_string(*args, **kwargs):
+        print("Mock get_secret_string called")
+        return json.dumps({"domain": "https://acmecorp.codeocean.com", "token": "fake_token"})
 
 class TestCredentialsInAWS(unittest.TestCase):
     """Tests credentials in AWS methods."""
-
-    def test__get_secret():
+    @mock_secretsmanager
+    @mock_sts
+    def test__get_secret(self):
         """Tests _get_secret method."""
-        pass
+        domain = "https://acmecorp.codeocean.com"
+        token = "fake_token"
+        # 
+        fake_sm = boto3.client("secretsmanager", region_name="us-west-2")
+        mock_secret = fake_sm.create_secret(
+            Name="codeocean-service-account",
+            SecretString=json.dumps({"domain": "https://acmecorp.codeocean.com", "token": "fake_token"})
+        )
+        print("Mock secret created: " + json.dumps(mock_secret))
+
+        # Mock _get_secrets_cache_client() method to return the fake secretsmanager
+        with patch.object(credentials_in_aws, "_get_secrets_cache_client") as mock_get_secrets_cache_client:
+            mock_get_secrets_cache_client.return_value = MockSecretCache()
+            secret = credentials_in_aws._get_secret(secret_name="codeocean-service-account")
+        
+        self.assertEqual(secret, json.dumps({"domain": domain, "token": token}))
 
     @patch("json.dump")
     @patch("builtins.open", new_callable=unittest.mock.mock_open())
@@ -29,6 +58,10 @@ class TestCredentialsInAWS(unittest.TestCase):
         domain = "https://acmecorp.codeocean.com"
         token = "fake_token"
         
+        # Create a mock for the _get_secret method that returns the fake credentials from a mocked secretsmanager
+        with patch.object(credentials_in_aws, "_get_secret") as mock_get_secret:
+            mock_get_secret.return_value = {"domain": domain, "token": token}
+
         credentials_in_aws.create_credentials_from_aws_secrets_manager(
             secret_name="test_secret",
             file_location="mock_file.json"
